ec97fdc884c53b608ea3ce244e8da09160a965df
pierluigi-ruffolo <pierluigiruffolo1@gmail.com>
2026-01-08 13:38:22 +0100

    feat: implemento middleware errorHandler, not Found

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 import express from "express";
 import routerPosts from "./routers/posts.js";
-
+import notFound from "./middlewares/notFound.js";
+import errorHandler from "./middlewares/errorHandler.js";
 const app = express();
 const port = 3000;
 
 app.use(express.static("public"));
 app.use(express.json());
 app.use("/posts", routerPosts);
+
+app.use(errorHandler);
+app.use(notFound);
 
 app.listen(port, () => {
   console.log("Server avviato sulla porta" + " " + port);
---
+++ middlewares/notFound.js
@@ -0,0 +1,7 @@
+export default function notFound(req, res, next) {
+  res.status(404);
+  res.json({
+    error: "Not Found",
+    message: "Pagina non trovata",
+  });
+}
